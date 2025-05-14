c4b09116f7d190fec3633f4868302fa10d77c944
Richard Smythe <richard_smythe@gallagherre.com>
2025-05-14 14:54:49 +0100

    Update

--- bpe/bpe.c
+++ bpe/bpe.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <stdint.h>
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
 
@@ -10,23 +10,23 @@ typedef struct {
 	size_t capacity;
 	size_t item_size;
 	void* items;
-}Array;
+} Array;
 
-Array array_init(size_t item_size, size_t initial_capacity)
-{
-	return(Array) {
-		.capacity = initial_capacity,
-		.item_size = item_size,
-		.items = malloc(item_size * initial_capacity),
+Array array_init(size_t item_size, size_t initial_capacity) {
+	return (Array) {
+		.length = 0,
+			.capacity = initial_capacity,
+			.item_size = item_size,
+			.items = malloc(item_size * initial_capacity)
 	};
 }
 
-Array array_append(Array* a, void* item) {
+void array_append(Array* a, void* item) {
 	if (a->length >= a->capacity) {
 		a->capacity *= 2;
 		void* new_items = realloc(a->items, a->item_size * a->capacity);
 		if (!new_items) {
-			fprintf(stderr,"error appending to array\n");
+			fprintf(stderr, "error appending to array\n");
 			exit(1);
 		}
 		a->items = new_items;
@@ -37,37 +37,40 @@ Array array_append(Array* a, void* item) {
 }
 
 typedef struct {
-	char pair[2];
-}Pair;
+	uint32_t pair[2];
+} Pair;
+
+uint32_t next_token_id = 256;
+uint32_t* merge_map = NULL;
 
 typedef struct {
 	Pair key;
 	size_t value;
-}Freq;
+} Freq;
 
 Freq* freq = NULL;
-
-//typedef struct {
-//	Freq* items
-//}Freqs;
-//
 
 int compare_freqs(const void* a, const void* b) {
 	const Freq* af = a;
 	const Freq* bf = b;
-	return	(int)bf->value - (int)af->value;
+	return (int)(bf->value - af->value);
 }
 
 int main() {
 	const char* text = "The original BPE algorithm operates by iteratively replacing the most common contiguous sequences of characters in a target text with unused 'placeholder' bytes. The iteration ends when no sequences can be found, leaving the target text effectively compressed. Decompression can be performed by reversing this process, querying known placeholder terms against their corresponding denoted sequence, using a lookup table. In the original paper, this lookup table is encoded and stored alongside the compressed text.";
 	int text_size = strlen(text);
 
-	// count frequencies
-	for (int i = 0; i < text_size - 1; i++)
-	{
-		Pair pair = {
-			.pair = { text[i], text[i + 1] }
-		};
+	Array tokens = array_init(sizeof(uint32_t), text_size);
+	for (size_t i = 0; i < strlen(text); i++) {
+		uint32_t token = (uint8_t)text[i];
+		array_append(&tokens, &token);
+	}
+
+
+	uint32_t* token_items = (uint32_t*)tokens.items;
+
+	for (size_t i = 0; i < tokens.length - 1; i++) {
+		Pair pair = { .pair = { token_items[i], token_items[i + 1] } };
 
 		ptrdiff_t index = hmgeti(freq, pair);
 		if (index < 0) {
@@ -78,23 +81,21 @@ int main() {
 		}
 	}
 
-
-	// copy to crappy array
 	Array sorted_freqs = array_init(sizeof(Freq), 16);
 	for (ptrdiff_t i = 0; i < hmlen(freq); i++) {
 		array_append(&sorted_freqs, &freq[i]);
 	}
-	
-	// sort by value
-	qsort(sorted_freqs.items, sorted_freqs.length, sizeof(Freq), compare_freqs);
-	for (size_t i = 0; i < 10; i++)
-	{
-		Freq* f = &((Freq*)sorted_freqs.items)[i];
-		printf("%c%c => %zu\n", f->key.pair[0], f->key.pair[1], f->value);
 
+	qsort(sorted_freqs.items, sorted_freqs.length, sizeof(Freq), compare_freqs);
+
+	for (size_t i = 0; i < 10 && i < sorted_freqs.length; i++) {
+		Freq* f = &((Freq*)sorted_freqs.items)[i];
+		printf("[%u %u] => %zu\n", (char)f->key.pair[0], (char)f->key.pair[1], f->value);
 	}
 
 	free(sorted_freqs.items);
+	free(tokens.items);
+	hmfree(freq);
 
 	return 0;
 }
